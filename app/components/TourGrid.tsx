9958ee4f0f5c5583ad938e29bfefd0a00ffcfc84
Ojaybit <joshuaben3030@gmail.com>
2026-02-02 09:39:49 +0100

    img

--- app/components/TourGrid.tsx
+++ app/components/TourGrid.tsx
@@ -10,7 +10,7 @@ const destinations = [
   { id: 2, title: "Sharm El-Sheikh", image: "aswan.jpg", slug: "classic-nile-cruise-experience" },
   { id: 3, title: "Cairo", image: "cairo.jpg", slug: "explore-egypt-from-the-pyramids-to-the-white-desert" },
   { id: 4, title: "Marsa Allam", image: "marsa-allam.jpg", slug: "luxury-siwa-oasis-white-desert-adventure" },
-  { id: 5, title: "Al Fayom", image: "Fayom.jpg", slug: "cairo-alexandria-highlights-tour" },
+  { id: 5, title: "Al Fayom", image: "fayom.jpg", slug: "cairo-alexandria-highlights-tour" },
   { id: 6, title: "Siwa", image: "siwa.jpg", slug: "red-sea--desert-safari-tour" },
   { id: 7, title: "Aswan", image: "aswan.jpg", slug: "ancient-egypt-discovery-tour" },
   { id: 8, title: "Hurghada", image: "hurghada.jpg", slug: "egypt-cultural-heritage-tour" }, // new mixing card
