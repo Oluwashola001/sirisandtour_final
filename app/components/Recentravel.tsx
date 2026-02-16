672e2aadd224d8d3cec04550af8eb72fde2f4ca9
Ojaybit <joshuaben3030@gmail.com>
2026-02-16 15:07:12 +0100

    changed Recenttravel imgs

--- app/components/Recentravel.tsx
+++ app/components/Recentravel.tsx
@@ -6,20 +6,20 @@ import { motion, AnimatePresence } from 'framer-motion';
 /* ---------------- IMAGES ---------------- */
 
 const images = [
-  '/images/gallery/1.jpg',
-  '/images/gallery/2.jpg',
-  '/images/gallery/3.jpg',
-  '/images/gallery/4.jpg',
-  '/images/gallery/5.jpg',
-  '/images/gallery/6.jpg',
-  '/images/gallery/7.jpg',
-  '/images/gallery/8.jpg',
-  '/images/gallery/9.jpg',
-  '/images/gallery/10.jpg',
-  '/images/gallery/11.jpg',
-  '/images/gallery/12.jpg',
-  '/images/gallery/13.jpg',
-  '/images/gallery/14.jpg',
+  '/images/Recenttravel/1.jpg',
+  '/images/Recenttravel/2.jpg',
+  '/images/Recenttravel/3.jpg',
+  '/images/Recenttravel/4.jpg',
+  '/images/Recenttravel/5.jpg',
+  '/images/Recenttravel/6.jpg',
+  '/images/Recenttravel/7.jpg',
+  '/images/Recenttravel/8.jpg',
+  '/images/Recenttravel/9.jpg',
+  '/images/Recenttravel/10.jpg',
+  '/images/Recenttravel/11.jpg',
+  '/images/Recenttravel/12.jpg',
+  '/images/Recenttravel/13.jpg',
+  '/images/Recenttravel/14.jpg',
 ];
 
 /* ---------------- TYPES ---------------- */
