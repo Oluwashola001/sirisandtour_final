50ef9eb07b4ed6df56ec082e63c94eeed461b2cc
OJaybit <joshuaben3030@gmail.com>
2026-03-02 01:50:47 +0100

    Simplify RootLayout by removing unused components
    Removed layout components including Navbar, WhatsAppButton, ScrollToTop, Footer, and GoogleTranslate.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,4 @@
 import "./globals.css";
-import Navbar from "./components/Navbar";
-import WhatsAppButton from "./components/WhatsAppButton";
-import ScrollToTop from "./components/ScrollToTop";
-import Footer from "./components/Footer";
-import GoogleTranslate from "./components/GoogleTranslate";
 
 export default function RootLayout({
   children,
@@ -12,20 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="min-h-screen overflow-x-hidden flex flex-col">
-        <Navbar />
-
-        {/* Language dropdown UI */}
-        <div className="fixed top-6 right-6 z-[9999]">
-          <GoogleTranslate />
-        </div>
-
-        <WhatsAppButton />
-        <ScrollToTop />
-
-        <main className="flex-1">{children}</main>
-        <Footer />
-      </body>
+      <body />
     </html>
   );
 }
