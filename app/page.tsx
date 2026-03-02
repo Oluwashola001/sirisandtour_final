daee3c2e41d0992c37f5350911b70f07232319f6
OJaybit <joshuaben3030@gmail.com>
2026-03-02 01:42:02 +0100

    Remove all components from HomePage
    Commented out all component imports and removed JSX return.

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,20 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import TourGrid from './components/TourGrid';
-import EndlessWonder from './components/EndlessWonder';
-import TourCategories from './components/TourCategories';
-import Professionalguide from './components/Professionalguide'
-import PopularDestinations from './components/PopularDestinations'
-import BackgroundSection from './components/BackgroundSection';
-import Recentravel from './components/Recentravel'
-import Tourbgsection from './components/Tourbgsection'
-import StatsCircles from './components/StatsCircles'
-import Explore from './components/Explore'
-import Animation from './components/Animation'
-import Newsletter from './components/Newsletter'
-import Testimonials from "./components/Testimonials"
-import ComingSoon from './components/ComingSoon';
+// import Navbar from './components/Navbar';
+// import Hero from './components/Hero';
+// import TourGrid from './components/TourGrid';
+// import EndlessWonder from './components/EndlessWonder';
+// import TourCategories from './components/TourCategories';
+// import Professionalguide from './components/Professionalguide';
+// import PopularDestinations from './components/PopularDestinations';
+// import BackgroundSection from './components/BackgroundSection';
+// import Recentravel from './components/Recentravel';
+// import Tourbgsection from './components/Tourbgsection';
+// import StatsCircles from './components/StatsCircles';
+// import Explore from './components/Explore';
+// import Animation from './components/Animation';
+// import Newsletter from './components/Newsletter';
+// import Testimonials from "./components/Testimonials";
+// import ComingSoon from './components/ComingSoon';
 
 export default function HomePage() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <Tourbgsection/>
-      <TourGrid />
-      <EndlessWonder />
-      <TourCategories />
-      <Professionalguide/>
-      <BackgroundSection />
-            <PopularDestinations/>
-      <ComingSoon />
-      <Recentravel/>
-      <StatsCircles/>
-        <Testimonials />
-      <Animation/>
-      <Explore/>
-      <Newsletter/>
-    </>
-  );
+  return null;
 }
