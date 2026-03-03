096009c608436616ab536e53b20a9719624e120b
OJaybit <joshuaben3030@gmail.com>
2026-03-03 08:56:18 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,9 @@
 import "./globals.css";
+import Navbar from "./components/Navbar";
+import WhatsAppButton from "./components/WhatsAppButton";
+import ScrollToTop from "./components/ScrollToTop";
+import Footer from "./components/Footer";
+import GoogleTranslate from "./components/GoogleTranslate";
 
 export default function RootLayout({
   children,
@@ -7,7 +12,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body />
+      <body className="min-h-screen overflow-x-hidden flex flex-col">
+        <Navbar />
+
+        {/* Language dropdown UI */}
+        <div className="fixed top-6 right-6 z-[9999]">
+          <GoogleTranslate />
+        </div>
+
+        <WhatsAppButton />
+        <ScrollToTop />
+
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
